4667b3b595de70add183034fb38e7635d6b7629d
phongbinht <phongbinht@synology.com>
2024-01-17 15:57:29 +0800

    feat: input output filepaths

--- main.cpp
+++ main.cpp
@@ -9,12 +9,17 @@
 #include <winerror.h> // WINdows Error
 
 #include <iostream>
+#include <cstdlib>
 
-#define SRC_FILEPATH L"C:\\Users\\phongbinht\\Desktop\\heic\\IMG_4892.HEIC"
-#define DST_FILEPATH L"C:\\Users\\phongbinht\\Desktop\\heic\\IMG_4892.JPG"
+#define MAXIMUM_FILEPATH_LENGTH 32767
 #define MAXIMUM_THUMBNAIL_SIZE_PIXELS 2160
 
-int main() {
+int main(int argc, char* argv[]) {
+  wchar_t kSrcFilepath[MAXIMUM_FILEPATH_LENGTH];
+  std::mbstowcs(kSrcFilepath, argv[1], MAXIMUM_FILEPATH_LENGTH);
+  wchar_t kDstFilepath[MAXIMUM_FILEPATH_LENGTH];
+  std::mbstowcs(kDstFilepath, argv[2], MAXIMUM_FILEPATH_LENGTH);
+
   HRESULT kResult; // https://learn.microsoft.com/zh-tw/windows/win32/seccrypto/common-hresult-values
 
   kResult = CoInitializeEx(NULL, // https://learn.microsoft.com/en-us/windows/win32/api/combaseapi/nf-combaseapi-coinitializeex
@@ -25,7 +30,7 @@ int main() {
   }
 
   IShellItem* pItem; // https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nn-shobjidl_core-ishellitem
-  kResult = SHCreateItemFromParsingName(SRC_FILEPATH, // https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-shcreateitemfromparsingname
+  kResult = SHCreateItemFromParsingName(kSrcFilepath, // https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-shcreateitemfromparsingname
                                         NULL,
                                         IID_PPV_ARGS(&pItem)); // https://learn.microsoft.com/en-us/windows/win32/api/combaseapi/nf-combaseapi-iid_ppv_args
   if (FAILED(kResult)) {
@@ -59,7 +64,7 @@ int main() {
 
   CImage kImage; // https://learn.microsoft.com/en-us/cpp/atl-mfc-shared/reference/cimage-class?view=msvc-170
   kImage.Attach(kBitmap);
-  kResult = kImage.Save(DST_FILEPATH,
+  kResult = kImage.Save(kDstFilepath,
                         Gdiplus::ImageFormatJPEG); // https://learn.microsoft.com/zh-tw/windows/win32/gdiplus/-gdiplus-constant-image-file-format-constants
   if (FAILED(kResult)) {
     std::cout << "CImage::Save failed with error code " << kResult << std::endl;
